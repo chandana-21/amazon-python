2509e267c010da57813472de2b6d0ec3824269b0
chandana-21 <chandanakuntala21@gmail.com>
2021-12-02 20:05:20 +0530

    first commit

---
+++ scraper.py
@@ -0,0 +1,6 @@
+import requests
+
+URL = 'https://www.amazon.in/dp/1791392792/?coliid=I1Q6Q1GN90M81Q&colid=34HQJ9T6JADS3&psc=1&ref_=lv_ov_lig_dp_it'
+
+headers = {"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15'}
+
