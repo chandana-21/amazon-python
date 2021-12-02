7c729652e12c19a9d0a4744d994930dc6ad21cf8
chandana-21 <chandanakuntala21@gmail.com>
2021-12-02 20:58:42 +0530

    code pushed

--- scraper.py
+++ scraper.py
@@ -1,13 +1,29 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import smtplib
 
 URL = 'https://www.amazon.in/dp/1791392792/?coliid=I1Q6Q1GN90M81Q&colid=34HQJ9T6JADS3&psc=1&ref_=lv_ov_lig_dp_it'
 
 headers = {"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15'}
 
-page = requests.get(URL, headers=headers)
+def check_price():
 
-soup = bs(page.content, 'html.parser')
+    page = requests.get(URL, headers=headers)
 
-title = soup.find(id='productTitle').get_text()
-print(title)
+    soup = bs(page.content, 'html.parser')
+
+    title = soup.find(id='productTitle').get_text()
+    price = soup.find("span", {'class':'offer-price'}).get_text()
+    int_price = int(price[1:2]+price[3:5])*10
+    print(int_price)
+
+    if int_price < 800:
+        send_mail()
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo() 
+    server.starttls()
+    server.ehlo()
+
+    server.login('mailsofck')
